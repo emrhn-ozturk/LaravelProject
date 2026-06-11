57044ada61305dd626624125caeb7b9e0754e105
Emirhan Öztürk <emrhnozturk.2003@gmail.com>
2026-06-11 16:39:31 +0300

    Create models, migrations, and controller for Travel Tech categories and products

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id', 'user_id', 'name', 'description', 'price', 'stock', 'image'
+    ];
+
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
